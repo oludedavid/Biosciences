4837c0666ae1ba599b56fccde57c9fabfae786a3
Olude david <odavidolumide@gmail.com>
2022-11-14 18:53:20 +0100

    loader

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,8 @@
 const navigation = new Navigation(document.getElementById("navigation"));
+
+$(window).on("load", function () {
+  setTimeout(function () {
+    // allowing 3 secs to fade out loader
+    $(".page-loader").fadeOut("slow");
+  }, 3500);
+});

d8b48cca0c7825386bd168334a5b9f17bf2917ed
Olude david <odavidolumide@gmail.com>
2022-11-14 16:27:18 +0100

    changed layout

--- application.py
+++ application.py
@@ -11,38 +11,6 @@ from flask_mail import Mail, Message
 application = Flask(__name__)
 
 
-# # path
-# basedir = os.path.abspath(os.path.dirname(__file__))
-# # initialise the database with sqlalchemy
-# # mysql://username:password@host:port/database_name
-# application.config[
-#     "SQLALCHEMY_DATABASE_URI"
-# ] = "mysql://root:OLUDEdavid1995@localhost:3306/bio-medic"
-# application.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-# flask mail
-application.config["DEBUG"] = True
-application.config["TESTING"] = False
-application.config["MAIL_SERVER"] = "smtp.gmail.com"
-application.config["MAIL_PORT"] = 587
-application.config["MAIL_USERNAME"] = "odavidolumide@gmail.com"
-application.config["MAIL_PASSWORD"] = ""
-application.config["MAIL_USE_TLS"] = False
-application.config["MAIL_USE_SSL"] = True
-application.config["MAIL_ASCII_ATTACHMENTS"] = False
-application.config["MAIL_DEFAULT_SENDER"] = None
-
-
-# db = SQLAlchemy(application)
-application.config["GOOGLEMAPS_KEY"] = "8JZ7i18MjFuM35dJHq70n3Hx4"
-
-# Initialize the extension
-GoogleMaps(application)
-
-# Flask Mail
-mail = Mail(application)
-
-
 @application.route("/map")
 def mapview():
 
--- static/js/home.js
+++ static/js/home.js
@@ -4,5 +4,23 @@ $(document).ready(function () {
     arrowLeft: "<i class='fa-regular fa-circle-left fa-3x'></i>",
     arrowRight: "<i class='fa-regular fa-circle-right fa-3x'></i>",
     arrowElement: "<button style='background-color: white;'></button>",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
   });
 });
